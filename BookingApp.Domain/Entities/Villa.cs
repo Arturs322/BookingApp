a61c4772fe0dbcf8c1f95733021dec18f116126e
Arturs Krusts <arturs@atmosclear.com>
2024-01-08 15:49:28 +0200

    Add project files.

---
+++ BookingApp.Domain/Entities/Villa.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingApp.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
