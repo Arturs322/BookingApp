ff4ae82535241c18b0ab9ed73cbc24911ead63b9
Arturs Krusts <arturs@atmosclear.com>
2024-01-10 15:36:16 +0200

    create VillaNumber model

---
+++ BookingApp.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingApp.Domain.Entities
+{
+    public class VillaNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DisplayName("Villa Number")]
+        public int Villa_Number { get; set; }
+        [DisplayName("Villa ID")]
+        public int VillaId { get; set; }
+        [ForeignKey("VillaId")]
+        [ValidateNever]
+        public Villa Villa { get; set; }
+        [DisplayName("Special Details")]
+        public string? SpecialDetails { get; set; }
+    }
+}
